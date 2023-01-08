e219b300c667db19183176306f1d5413a2c6fefe
Kazutadashi <Kazutadashi@yahoo.com>
2023-01-08 11:01:29 -0700

    Add toggle for rush mode and convert run into func

--- scripts/puzzle_trainer.py
+++ scripts/puzzle_trainer.py
@@ -15,8 +15,47 @@ def get_puzzle(puzzle_link):
     else:
         return False
 
-# with open('lichess-tactics-assistant\data\puzzle_links.txt') as file:
-#     puzzle_links = [line.rstrip() for line in file]
+def start_run(rush_mode=False):
+    numberCorrect = 0
+    numberFailed = 0
+    puzzlesAttempted = 0
+    numberOfPuzzles = len(puzzle_links)
+    
+    start_time = time.time()
+    while len(puzzle_links) > 0:
+        
+        # pick a random puzzle
+        random_list_index = random.randint(0, len(puzzle_links)-1)
+        current_puzzle = puzzle_links[random_list_index]
+
+        puzzle_result = get_puzzle(current_puzzle)
+        
+        if puzzle_result == True:
+            numberCorrect+=1
+        else:
+            numberFailed+=1
+        puzzlesAttempted += 1
+        
+        # wait until key is pressed to go to next puzzle
+        if rush_mode == False:
+            keyboard.wait("a")
+            print("'A' key was pressed. Going to the next puzzle.")
+
+        print(f"[{puzzlesAttempted}/{numberOfPuzzles}] - {numberCorrect} Correct | {numberFailed} Failed.")
+        
+        puzzle_links.pop(random_list_index)
+        end_time = time.time()
+
+    time_taken = end_time - start_time
+    time_format = "%M:%S"
+    print(time_taken)
+    print(f"-----------------\nCycle finished.\n\nAccuracy: {round(numberCorrect/numberOfPuzzles*100, 2)}%, \
+        Time: {round((time_taken))} seconds. ({time.strftime(time_format, time.gmtime(time_taken))} minutes)")
+    print(f"Goal time is 180 seconds. {time_taken-180} off goal.")
+
+
+with open('lichess-tactics-assistant\data\puzzle_links.txt') as file:
+    puzzle_links = [line.rstrip() for line in file]
 
 puzzle_links = ["https://lichess.org/training/cxhfh",
                 "https://lichess.org/training/I8tZV",
@@ -27,43 +66,12 @@ puzzle_links = ["https://lichess.org/training/cxhfh",
 options = Options()
 options.add_argument("--user-data-dir=C:\\Users\\Kazutadashi\\Documents\\Programming\\repos\\lichess-tactics-assistant\\UserData")
 options.page_load_strategy = 'normal'
-
 driver = webdriver.Chrome(executable_path='C:\\Users\\Kazutadashi\\Documents\\Programming\\webdrivers\\chromedriver.exe', options=options)
 
-numberCorrect = 0
-numberFailed = 0
-puzzlesAttempted = 0
-numberOfPuzzles = len(puzzle_links)
-
-start_time = time.time()
-while len(puzzle_links) > 0:
-    
-    # pick a random puzzle
-    random_list_index = random.randint(0, len(puzzle_links)-1)
-    current_puzzle = puzzle_links[random_list_index]
-
-    puzzle_result = get_puzzle(current_puzzle)
-    
-    if puzzle_result == True:
-        numberCorrect+=1
-    else:
-        numberFailed+=1
-    puzzlesAttempted += 1
-    
-    # wait until key is pressed to go to next puzzle
-    keyboard.wait("a")
-    print("'A' key was pressed. Going to the next puzzle.")
-    print(f"[{puzzlesAttempted}/{numberOfPuzzles}] - {numberCorrect} Correct | {numberFailed} Failed.")
-    
-
-    puzzle_links.pop(random_list_index)
 
 
-end_time = time.time()
-time_taken = end_time - start_time
-time_format = "%M:%S"
-print(time_taken)
-print(f"-----------------\nCycle finished.\n\nAccuracy: {round(numberCorrect/numberOfPuzzles*100, 2)}%, \
-    Time: {round((time_taken))} seconds. ({time.strftime(time_format, time.gmtime(time_taken))} minutes)")
-print(f"Goal time is 180 seconds. {time_taken-130} off goal.")
+
+
+
+start_run(rush_mode=True)
 driver.close()
